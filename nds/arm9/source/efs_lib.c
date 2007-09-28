f455cff3fc25bd36821796c5ae29a816b50e1204
chuckstudios <chuckstudios@ebfc4fc4-5a3a-0410-bdef-d5cd533c4aa1>
2007-09-28 22:43:16 +0000

    nds: corrected a small error in EFSlib where seeking past the end of the file would do bad things
    win: cleaned up some source, added code for saving the PSRAM/Compression options
    
    modified   nds/arm9/source/efs_lib.c
    modified   win/frmNDStation.frm

--- nds/arm9/source/efs_lib.c
+++ nds/arm9/source/efs_lib.c
@@ -362,7 +362,7 @@ extern int EFS_fread(void *buffer, u32 blockSize, u32 numBlocks, EFS_FILE *file)
 	int tempReturn = 0;
 	fseek(nds_file, file->seek_pos, SEEK_SET);
 	tempReturn = fread(buffer, blockSize, numBlocks, nds_file);
-	file->seek_pos += blockSize * numBlocks;
+	file->seek_pos += blockSize * tempReturn;
 	return tempReturn;
 }
 
@@ -372,7 +372,7 @@ extern int EFS_fwrite(void *buffer, u32 blockSize, u32 numBlocks, EFS_FILE *file
 	int tempReturn = 0;
 	fseek(nds_file, file->seek_pos, SEEK_SET);
 	tempReturn = fwrite(buffer, blockSize, numBlocks, nds_file);
-	file->seek_pos += blockSize * numBlocks;
+	file->seek_pos += blockSize * tempReturn;
 	return tempReturn;
 }
 
