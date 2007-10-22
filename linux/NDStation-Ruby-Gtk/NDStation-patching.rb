8b0b0c695678846690771859fcbecc395a5cbb7a
4zimuth <4zimuth@ebfc4fc4-5a3a-0410-bdef-d5cd533c4aa1>
2007-10-22 21:28:01 +0000

    ruby/gtk

--- linux/NDStation-Ruby-Gtk/NDStation-patching.rb
+++ linux/NDStation-Ruby-Gtk/NDStation-patching.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #NDStation-patching handles individual file patching in NDStation
 
-require 'open-uri' #to grab files from the net
+require 'open-uri'
 
 #downloads files from the internet
 def download(file_name, webloc)
@@ -9,19 +9,21 @@ def download(file_name, webloc)
   open(file_name, "w").write(open(webloc).read)
 end
 
-unless File.exist?("ndstool") and File.exist?("9.bin") and File.exist?("7.bin") #test for file existence(duh)
-  print "ndstool, 7.bin or 9.bin don't exist, grab them from the interent[y/N]? "
-  answer = gets.to_s.chomp!
-  if answer == "y"
-    download("ndstool", "http://foobarbaz.com/ndstool")
-    download("7.bin", "http://foobarbaz.com/7.bin")
-    download("9.bin", "http://foobarbaz.com/9.bin")
+def check
+  unless File.exist?("ndstool") and File.exist?("9.bin") and File.exist?("7.bin") #test for file existence(duh)
+    print "ndstool, 7.bin or 9.bin don't exist, grab them from the interent[y/N]? "
+    answer = gets.to_s.chomp!
+    if answer == "y"
+      download("ndstool", "http://foobarbaz.com/ndstool")
+      download("7.bin", "http://foobarbaz.com/7.bin")
+      download("9.bin", "http://foobarbaz.com/9.bin")
+    else
+      puts "Exiting program"
+      exit
+    end
   else
-    puts "Exiting program"
-    exit
+    puts "Files exist"
   end
-else
-  puts "Files exist"
 end
 
 class Rom
@@ -31,8 +33,36 @@ class Rom
     @icon = icon
     @title = title
   end
-  
   def patch
-    %x(./ndstool -c #@file_name -7 7.bin -9 9.bin -d #@path -g NDST -b #@icon #@title)
+    print "Do you really want to patch this file[y/N]? "
+    ans = gets.to_s.chomp!
+    if ans == "y"
+      %x(./ndstool -c #@file_name -7 7.bin -9 9.bin -d #@path -g NDST -b #@icon #@title)
+    else
+      puts "Exiting"
+    end
   end
+  
+  def extract(file_name) #split filename to string and extension, compares extension
+    reg = file_name.split('.', 2) #and executes command to extract
+    case
+    when reg.include?('rar')
+      %x(unrar x #@filename)
+    when reg.include?('zip')
+      %x(unzip #@filename)
+    when reg.include?('tar.gz')
+      %x(tar -xf #@filename)
+    when reg.include?('tar.bz2')
+      %x(tar -xf #@filename)
+    when reg.include?('7z')
+      %x(p7zip #@filename)
+    else
+      puts "File is not compressed"
+    end
+  end
+end
+
+if __FILE__ == $0 #irb
+  #checker
+  #Rom.new("file_name", "path", "icon.png", "title").extract.patch
 end
