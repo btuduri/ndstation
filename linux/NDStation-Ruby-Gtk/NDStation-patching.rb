d1b6726abb73cd0d3eb95fe68c1a57ba3d78823e
4zimuth <4zimuth@ebfc4fc4-5a3a-0410-bdef-d5cd533c4aa1>
2007-10-30 00:07:55 +0000

    Re-wrote the gui so that the main window is a child class of Gtk::Window, that helped clean up the code.

--- linux/NDStation-Ruby-Gtk/NDStation-patching.rb
+++ linux/NDStation-Ruby-Gtk/NDStation-patching.rb
@@ -8,34 +8,22 @@ def download(file_name, webloc)
   open(file_name, "w").write(open(webloc).read) {|file_name| puts "Downloading #{file_name}"}
 end
 
-def check
+def file_check
   unless File.exist?("ndstool") and File.exist?("9.bin") and File.exist?("7.bin") #test for file existance(duh :P)
-    print "ndstool, 7.bin or 9.bin don't exist, grab them from the interent[y/N]? "
-    answer = gets.to_s.chomp!
-    if answer == "y"
-      download("ndstool", "http://foobarbaz.com/ndstool")
-      download("7.bin", "http://foobarbaz.com/7.bin")
-      download("9.bin", "http://foobarbaz.com/9.bin")
-    else
-      puts "Exiting program"
-      exit
-    end
-  else
-    exec_path =  Dir.entries("/usr/bin")  #checks for 7z, unrar and uzip executables in /usr/bin
-    unless exec_path.include?("7z") and exec_path.include?("unrar") and exec_path.include?("unzip")
-      puts "please make sure you have 7z, unrar and unzip installed"
-      exit
-    end
+    1
+  end
+end
+
+def exec_check
+  unless Dir.entries('/usr/bin').include?('7z') and Dir.entries('/usr/bin').include?('unrar') and Dir.entries('/usr/bin').include?('zip')
+    1
   end
 end
 
 class Rom
   attr_accessor :file_name, :path, :icon, :title
   def initialize(file_name, path, icon, title)
-    @file_name = file_name
-    @path = path
-    @icon = icon
-    @title = title
+    @file_name, @path, @icon, @title = file_name, path, icon, title
   end
   def patch
     print "Do you really want to patch this file[y/N]? "
@@ -47,8 +35,7 @@ class Rom
     end
   end 
   def extract                       #split filename to string and extension, compares extension
-    reg = file_name.split('.', 2)   #and executes command to extract file.
-    unless reg[1] == "gba"
+    reg = file_name.split('.')   #and executes command to extract file.
       case
       when reg.include?('rar')
         %x(unrar x #@filename)
@@ -60,13 +47,13 @@ class Rom
         %x(tar -xf #@filename)
       when reg.include?('7z')
         %x(7z e #@filename)
+      when rar.include?('gba')
+        puts "File is not compressed"
+        patch
+      else
+        puts "File format unkown"
+        exit
       end
-      rom.file_name = reg[0] + ".gba"
-    end
+    rom.file_name = reg[0] + ".gba"
   end
-end
-
-if __FILE__ == $0 #irb
-  check
-  #rom = Rom.new("file_name", "path", "icon.png", "title").extract.patch
 end
